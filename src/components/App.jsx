26707ff23b72a2387f00b4d525bcb70a50bb893d
Anastasiia-Rodzina <pototzkaja.n@gmail.com>
2023-12-28 14:24:41 +0200

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import FeedbackOptions from "../components/FeedbackOptions/FeedbackOptions"
+import Statistics from "./Statistics/Statistics";
+import Section from "./Section/Section";
+import Notification from "./Notification/Notification";
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+renderFeedback = option => {
+  this.setState((prevState) => ({
+  
+      [option]: prevState[option] + 1,
+
+}));
 };
+
+countTotalFeedback = () => {
+  const { good, neutral, bad } = this.state;
+  const total = good + neutral + bad;
+  return total;
+};
+
+countPositiveFeedbackPercentage = () => {
+  const { good } = this.state;
+  const total = this.countTotalFeedback();
+  return Math.round((good * 100) / total) || 0;
+}
+
+  render (){
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const percentage = this.countPositiveFeedbackPercentage();
+    return (
+    <>
+<Section title="Please leave feedback">
+  <FeedbackOptions 
+options={Object.keys(this.state)} 
+onLeaveFeedback={this.renderFeedback}/>
+</Section>
+
+<Section title="Statistics">
+  {total === 0 ?
+  <Notification message="There is no feedback"></Notification> 
+:
+<Statistics 
+good={good} 
+neutral={neutral} 
+bad={bad} 
+total={total} 
+positivePercentage={percentage}/>
+}
+</Section>
+</>
+    )
+  }
+}
---
+++ src/components/Section/Section.js
@@ -0,0 +1,12 @@
+import { Children } from 'react';
+import css from './Section.module.css';
+
+function Section({ title, children }) {
+  return (
+    <section className={css.section}>
+      <h2 className={css.title}>{title}</h2>
+      {children}
+    </section>
+  );
+}
+export default Section;
